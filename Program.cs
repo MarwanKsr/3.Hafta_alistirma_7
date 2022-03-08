9897f6b39a16c46312464a2838e1bb08604dbbec
Marwan <Marwan.kaseer1997@gmail.com>
2022-03-09 00:30:13 +0300

    Program is done

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace alistirma_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Odanizin sicakligi giriniz: ");
+            double sicaklik = double.Parse(Console.ReadLine());
+            double fahrenheit = (9/5f*sicaklik) +32;
+            Console.WriteLine("Odanizin Fahrenheit sicakligi {0}" ,fahrenheit);
+        }
+    }
+}
